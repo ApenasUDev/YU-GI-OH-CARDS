131ae0ffcc048a58ec7bc6a5fe4a875b1960273f
Francimar Alexandre <139023397+ApenasUDev@users.noreply.github.com>
2023-11-01 20:15:14 -0300

    criando o projeto + app

--- YugiohCardsProject/YugiohCardsProject/urls.py
+++ YugiohCardsProject/YugiohCardsProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for YugihoCardsProject project.
+URL configuration for YugiohCardsProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from YugiohCardsApp import views
 urlpatterns = [
+    path('',views.home,name="home"),
     path('admin/', admin.site.urls),
 ]
