8944df224818041eff2bb128bc276501c8110f1e
Francimar Alexandre <139023397+ApenasUDev@users.noreply.github.com>
2023-11-06 09:36:00 -0300

    exibindo as imagens dos cards

--- YugiohCardsProject/YugiohCardsApp/views.py
+++ YugiohCardsProject/YugiohCardsApp/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    base_url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+        
+    try:
+            response = requests.get(base_url)
+            response.raise_for_status()
+            data = response.json()
+            
+            cards = []  # Lista para armazenar os dados dos cards
+
+            if data["data"]:
+                for resultados in data["data"]:
+                    cards_info = {
+                        "image": resultados["card_images"][0]["image_url"],
+                    }
+                    cards.append(cards_info)
+
+                # Use slice para pegar apenas os primeiros 25 itens
+                cards = cards[:25]
+
+                contexto = {"cards": cards}
+    except requests.exceptions.RequestException as e:
+            print(f"Erro na solicitação HTTP: {e}")
+            contexto = {"cards": []}  # Lista vazia em caso de erro
+
+    return render(request, 'home.html', contexto)
