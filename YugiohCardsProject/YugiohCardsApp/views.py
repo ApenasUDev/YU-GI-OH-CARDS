2cdcde05897042aeff7068d737f14a730a18edfe
Francimar Alexandre <139023397+ApenasUDev@users.noreply.github.com>
2023-11-07 09:11:32 -0300

    sistema de buscar de cards completo

--- YugiohCardsProject/YugiohCardsApp/views.py
+++ YugiohCardsProject/YugiohCardsApp/views.py
@@ -51,3 +51,51 @@ def home(request):
             contexto = {"cards": []}  # Lista vazia em caso de erro
 
     return render(request, 'home.html', contexto)
+
+def buscar_card(request):
+    nome_card = request.GET.get('nome_card', '') 
+    BASE_URL = f"https://db.ygoprodeck.com/api/v7/cardinfo.php?name={nome_card}"
+    try:
+            response = requests.get(BASE_URL)
+            response.raise_for_status()
+            data = response.json()
+            
+            cards = []  # Lista para armazenar os dados dos cards
+
+            if data["data"]:
+                for resultados in data["data"]:
+                    if resultados["type"] == "Trap Card" or resultados["type"] == "Spell Card":
+                        cards_info = {
+                            "id": resultados["id"],
+                            "image": resultados["card_images"][0]["image_url"],
+                            "name": resultados["name"],
+                            "type": resultados["type"],
+                            "desc": resultados["desc"],                     
+                            "race": resultados["race"],
+                           
+                        }
+                    else:
+                        cards_info = {
+                            "id": resultados["id"],
+                            "image": resultados["card_images"][0]["image_url"],
+                            "name": resultados["name"],
+                            "type": resultados["type"],
+                            "desc": resultados["desc"],
+                            "atk": resultados.get("atk", None),  # Use get() para obter "atk" com um valor padrão se estiver ausente
+                            "def": resultados.get("def", None),  # Use get() para obter "def" com um valor padrão se estiver ausente
+                            "level": resultados.get("level", None),  # Use get() para obter "level" com um valor padrão se estiver ausente
+                            "race": resultados["race"],
+                            "attribute": resultados.get("attribute",None),
+                        }
+                    cards.append(cards_info)
+
+  
+
+                contexto = {"cards": cards}
+              
+ 
+    except requests.exceptions.RequestException as e:
+            print(f"Erro na solicitação HTTP: {e}")
+            contexto = {"cards": []}  # Lista vazia em caso de erro
+
+    return render(request, 'home.html', contexto)
--- YugiohCardsProject/YugiohCardsProject/urls.py
+++ YugiohCardsProject/YugiohCardsProject/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',views.home,name="home"),
+    path('buscar/',views.buscar_card,name="buscar"),
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
