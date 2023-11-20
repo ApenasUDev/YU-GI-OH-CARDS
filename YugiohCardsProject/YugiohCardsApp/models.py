a6ca12c262df225fc56a28199e81a2c10ac608c1
ApenasUDev <francimaralexandredt@gmail.com>
2023-11-20 18:29:34 -0300

    criando uma tabbela para os cards

--- YugiohCardsProject/YugiohCardsApp/models.py
+++ YugiohCardsProject/YugiohCardsApp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
+    
     nome = models.CharField(max_length=250)
     senha = models.CharField(max_length=25)
+class SeusCads(models.Model):
+    usuario_cad = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_card = models.CharField(max_length=100)
+
     
