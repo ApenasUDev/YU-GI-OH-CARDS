88d89a5c4d80250708522367e9f8399f625004a4
ApenasUDev <francimaralexandredt@gmail.com>
2023-11-23 17:33:37 -0300

    incindo a função remove

--- YugiohCardsProject/YugiohCardsApp/static/js/seucard.js
+++ YugiohCardsProject/YugiohCardsApp/static/js/seucard.js
@@ -3,11 +3,25 @@ function addToDeck(card) {
     var cardId = card.getAttribute("data-id-data");
     var cardImage = card.getAttribute("data-image-data");
     var cardName = card.getAttribute("data-nome-data");
-    // ... extrair outros dados conforme necessário
 
     // Adicionar à área de construção do deck
-    var deckBuilder = document.querySelector(".deck-builder");
+    var deckBuilder = document.querySelector("#deck");
     var cardDiv = document.createElement("div");
-    cardDiv.innerHTML = "<img src='" + cardImage + "' alt='" + cardName + "'>";
+    cardDiv.innerHTML = "<img src='" + cardImage + "' alt='" + cardName + "' data-id-data='" + cardId + "' onclick='removeFromDeck(event);'>";
     deckBuilder.appendChild(cardDiv);
+}
+
+function removeFromDeck(event) {
+    // Verificar se o clique foi em uma carta no deck
+    if (event.target.tagName === 'IMG') {
+        // Extrair o ID da carta
+        var cardId = event.target.getAttribute("data-id-data");
+
+        // Encontrar e remover a carta do deck
+        var deckBuilder = document.querySelector("#deck");
+        var cardToRemove = deckBuilder.querySelector("[data-id-data='" + cardId + "']");
+        if (cardToRemove) {
+            deckBuilder.removeChild(cardToRemove);
+        }
+    }
 }
